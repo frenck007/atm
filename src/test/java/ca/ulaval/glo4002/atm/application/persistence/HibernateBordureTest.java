36d305b3218486bdd0206b3e13ba146ff6b1f8f1
Francois Dionne <francois.dionne.3@ulaval.ca>
2017-11-17 11:35:53 -0500

    Test 1 - Hibernate OK

--- src/test/java/ca/ulaval/glo4002/atm/application/persistence/HibernateBordureTest.java
+++ src/test/java/ca/ulaval/glo4002/atm/application/persistence/HibernateBordureTest.java
@@ -1,21 +1,49 @@
 package ca.ulaval.glo4002.atm.application.persistence;
 
+import static org.junit.Assert.*;
+
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import ca.ulaval.glo4002.atm.application.jpa.EntityManagerProvider;
+import ca.ulaval.glo4002.atm.domain.accounts.Account;
+import ca.ulaval.glo4002.atm.domain.accounts.StandardAccount;
+import ca.ulaval.glo4002.atm.infrastructure.persistence.HibernateAccountRepository;
+
 public class HibernateBordureTest {
+
+    private static final double BALANCE = 345.00;
+    private static final int ACCOUNT_NUMBER = 222;
+
+    private static EntityManagerFactory entityManagerFactory;
+
+    private HibernateAccountRepository accountRepo;
 
     @BeforeClass
     public static void setUpClass() {
-        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("atm-test");
+        entityManagerFactory = Persistence.createEntityManagerFactory("atm-test");
+    }
+
+    @Before
+    public void setUp() {
+        EntityManagerProvider.setEntityManager(entityManagerFactory.createEntityManager());
+
+        accountRepo = new HibernateAccountRepository();
     }
 
     @Test
     public void test() {
+        Account accountTest = new StandardAccount(ACCOUNT_NUMBER, BALANCE);
 
+        new EntityManagerProvider().executeInTransaction(() -> accountRepo.persist(accountTest));
+        // accountRepo.persist(accountTest);
+
+        Account accountPersisted = accountRepo.findByNumber(ACCOUNT_NUMBER);
+
+        assertEquals(accountPersisted.getAccountNumber(), accountTest.getAccountNumber());
     }
-
 }
